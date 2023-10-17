86ac457cae0660bf212a6db433b6b19e8ca54d92
cristian <cristiandevar@gmail.com>
2023-10-17 13:52:31 -0300

    Se agrego carousel al home

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return view('galeria.index');
+});
 
 Route::get('/login/google', 
 [App\Http\Controllers\GoogleLoginController::class, 
