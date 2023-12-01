561ecc034ecf5ad7d54c171aac5de9a1829ce947
cristian <cristiandevar@gmail.com>
2023-12-01 08:45:16 -0300

    tratamos de descargar y utilizar las librerias

---
+++ public/js/cruds/datatable.js
@@ -0,0 +1,7 @@
+import DataTable from 'datatables.net-bs4';
+import 'datatables.net-responsive-bs4';
+import 'datatables.net-searchbuilder-bs4';
+import 'datatables.net-searchpanes-bs4';
+$('#tabla-productos').DataTable({
+    responsive: true,
+})
--- resources/views/panel/products/crud/index.blade.php
+++ resources/views/panel/products/crud/index.blade.php
@@ -119,24 +119,26 @@
 
 {{-- Importacion de Archivos CSS --}}
 @section('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css"> --}}
 @stop
 
 
 {{-- Importacion de Archivos JS --}}
 @section('js')
 {{-- <script src="https://code.jquery.com/jquery-3.7.0.js"></script> --}}
-<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+{{-- <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap4.min.js"></script> --}}
+{{-- <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script> --}}
+<script type="text/javascript" src="{{ asset('js/cruds/datatable.js') }}"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript" src="{{ asset('js/cruds/confirm-delete.js') }}"></script>
-<script>
+{{-- <script>
     $('#tabla-productos').DataTable({
-    responsive: true
-});
-</script>
+    responsive: true,
+})
+</script> --}}
 @stop
